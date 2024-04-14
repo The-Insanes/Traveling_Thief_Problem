6119dc6cdf4a0d41f497c87016d1feeb64baf6b3
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-04-13 22:37:37 -0400

    repair bugs

--- TTP/House.py
+++ TTP/House.py
@@ -1,21 +1,25 @@
-from Object import Object
+from Item import Item
 
 class House():
-    def __init__(self) -> None:
+    def __init__(self, x, y, index) -> None:
         self.__objects = []
         self.__total_value = 0
         self.__total_weight = 0
         self.__ratio = 0
-        self.__pos = {"x": 0, "y": 0}
+        self.__pos = {"x": x, "y": y}
+        self.__index = index
 
-    def add_object(self, new_object: Object) -> None:
+    def add_object(self, new_object: Item) -> None:
         self.__objects.append(new_object)
         self.__total_value += new_object.get_profit()
         self.__total_weight += new_object.get_weight()
         self.__ratio = self.__total_value / self.__total_weight
 
-    def delete_object(self, del_object: Object):
+    def delete_object(self, del_object: Item):
         self.__objects.remove(del_object)
+
+    def get_index(self) -> int:
+        return self.__index
 
     def get_ratio(self) -> float:
         return self.__ratio
@@ -24,4 +28,4 @@ class House():
         return self.__objects
 
     def get_pos(self) -> None:
-        return self.__pos 
+        return self.__pos
