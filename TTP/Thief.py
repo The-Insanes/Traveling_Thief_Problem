d2694766ffaec4675068cdb0b464b5501517027d
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-04-14 16:41:22 -0400

    new function (get_free_weight)

--- TTP/Thief.py
+++ TTP/Thief.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from .Backpack import Backpack
 from .Item import Item
-=======
-from Backpack import Backpack
-from Item import Item
->>>>>>> Knapsack
 
 class Thief():
     def __init__(self, v_max: float, v_min: float, max_weight: int) -> None:
@@ -19,11 +14,14 @@ class Thief():
             total_weight = self.__backpack.get_total_weight()
             max_weight = self.__backpack.get_max_weight()
             self.__v_act = self.__v_max - total_weight * ((self.__v_max - self.__v_min) / max_weight)
-
+            
             return True
 
         return False
     
+    def get_free_weight(self) -> int:
+        return self.__backpack.get_max_weight() - self.__backpack.get_total_weight()
+
     def move(self, x: int, y: int) -> None:
         self.__pos['x'] = x
         self.__pos['y'] = y
