3366db1d1b58ae0ec8c00ce69ef03494be1a9ef4
Slinking196 <matias.bugueno.b@mail.pucv.cl>
2024-06-07 01:45:15 -0400

    add news mutations strategies, FlipSwamp and ResettingSwamp

---
+++ TTP/mutations/FlipSwamp.py
@@ -0,0 +1,24 @@
+from .Mutation_Base import Mutation_Base
+from numpy import ndarray
+import random
+
+class FlipSwamp(Mutation_Base):
+    def execute_knapsack(self, sol: ndarray, item_list: list = None, max_capacity: int = 0) -> ndarray:
+        i = random.randint(0, len(sol) - 1)
+        sol[i] = 1 - sol[i]
+
+        return sol
+    
+    def execute_tsp(self, sol: ndarray) -> ndarray:
+        i, j = random.sample(range(len(sol)), 2)
+        sol[i], sol[j] = sol[j], sol[i]
+
+        return sol
+
+    def execute(self, child: tuple, mutate_ratio: float) -> tuple:
+        if random.random() < mutate_ratio:
+            mutatedRoute = self.execute_tsp(child[0])
+            mutatedObjects = self.execute_knapsack(child[1])
+
+            child = mutatedRoute, mutatedObjects 
+        return child
